5be5e474a260c90ba7296a4acbac708ed1072f38
PavloLuchyk <76796482+PavloLuchyk@users.noreply.github.com>
2021-09-09 19:11:43 +0300

    Added fields and constructors

--- Homework6(immutable class)/src/com/example/ImmutableClass.java
+++ Homework6(immutable class)/src/com/example/ImmutableClass.java
@@ -1,4 +1,11 @@
 package com.example;
 
-public class ImmutableClass {
+public final class ImmutableClass {
+    private final int x;
+    private final int y;
+
+    public ImmutableClass(int x, int y) {
+        this.x= x;
+        this.y = y;
+    }
 }
